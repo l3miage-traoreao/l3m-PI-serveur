48df59f7b0c584cec4105395860808af23e3fc9b
l3miage-bourchoh <l3miage-bourchoh@users.noreply.github.com>
2021-04-13 18:27:12 +0200

    Création de la ressource GET /api/users/

--- src/main/java/com/example/UserCRUD.java
+++ src/main/java/com/example/UserCRUD.java
@@ -18,4 +18,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserCRUD{
 
 	private DataSource dataSource;
+
+
+
+
+
+ @GetMapping("/")
+   String db(Map<String, Object> model, HttpServletResponse response) {
+   try (Connection connection = dataSource.getConnection()) { 
+        Statement stmt = connection.createStatement(); 
+        ResultSet rs = stmt.executeQuery("SELECT * FROM users");  
+   		ArrayList<User> L = new ArrayList<User>(); 
+        while (rs.next()) { 
+          User u = new User(); 
+          u.login = rs.getString("login"); 
+          u.age   = rs.getInt("age"); 
+          L.add(u); 
+}
+		return L; 
+      } catch (Exception e) { 
+        response.setStatus(500); 
+	try{
+		response.getOutputStream().print( e.getMessage() ); 
+        } catch (Exception e2) { 
+          System.err.println(e2.getMessage()); 
+	}		
+		System.err.println(e.getMessage()); 
+        return null; 
 }
